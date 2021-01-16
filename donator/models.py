95a7a5c36aad2bcc8c5cece55124e3b63ff57ce5
JAgrit20 <jagritacharya2020@gmail.com>
2021-01-17 01:16:14 +0530

    model modified

--- donator/models.py
+++ donator/models.py
@@ -4,13 +4,14 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Product(models.Model):
    title = models.CharField(max_length=200)
+   Name = models.CharField(max_length=200,default='SOME STRING')
    pub_date = models.DateTimeField() 
    body = models.TextField()
-   url = models.TextField()
-   image = models.ImageField(upload_to='images/')
-   icon = models.ImageField(upload_to='images/')
-   votes_total = models.IntegerField(default=1)
-   hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+#    url = models.TextField()
+   itemImage = models.ImageField(upload_to='images/')
+   Id = models.ImageField(upload_to='images/',default='SOME STRING')
+#    votes_total = models.IntegerField(default=1)
+   owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
 def  __str__(self):
     return self.title 
