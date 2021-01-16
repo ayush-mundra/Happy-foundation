caccf33379e3fbc96dfe8b2ed97f5e6f546bfeba
JAgrit20 <jagritacharya2020@gmail.com>
2021-01-16 23:53:56 +0530

    auth part done

--- ngo2/urls.py
+++ ngo2/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name='home'),
     path('accounts/', include('accounts.urls')),
-    path('donatorView/', include('donatorView.urls')),
+    # path('donator/', include('donator.urls')),
 ]
