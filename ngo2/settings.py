54891ce8e298c99173e336e81d5ef0bd1ffd05d3
JAgrit20 <jagritacharya2020@gmail.com>
2021-01-17 02:26:23 +0530

    basic changes

--- ngo2/settings.py
+++ ngo2/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # 'django.contrib.gis',
     'donator.apps.DonatorConfig',
     'ngo.apps.NgoConfig',
     'accounts.apps.AccountsConfig',
