461aab7a83188ae897a3b8ccc2a27fbea9cd74cf
JAgrit20 <jagritacharya2020@gmail.com>
2021-01-16 23:32:59 +0530

    edited setting.py and added home page route

--- ngo2/settings.py
+++ ngo2/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'donator.apps.DonatorConfig',
+    'ngo.apps.NgoConfig',
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
