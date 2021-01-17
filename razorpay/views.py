7ca70e9e320e64a4cb961581e5c5b9ab05079ad2
JAgrit20 <jagritacharya2020@gmail.com>
2021-01-17 13:28:53 +0530

    integrated razor pay

--- razorpay/views.py
+++ razorpay/views.py
@@ -4,15 +4,23 @@ from django.views.decorators.csrf import csrf_exempt
 
 
 def pay(request):
+    if request.method == "POST":
+        amount = 50000
+        order_currency = 'INR'
+        
+
+# client.order.create(amount=order_amount, currency=order_currency, receipt=order_receipt, notes=notes)
     # if request.method == "POST":
     #     name = request.POST.get('name')
     #     amount = 50000
 
-        # client = razorpay.Client(
-        #     auth=("rzp_test_9nmrK825fjo0Ym", "1f1icPZDRCKvac3lzpOmLSl1"))
+    # client = razorpay.Client(auth=("rzp_live_4jh8hMv539N0rm", "RtVNts9SZLXkCW4yeJ8e2Yro"))
 
-        # payment = client.order.create({'amount': amount, 'currency': 'INR',
-        #                                'payment_capture': '1'})
+    client = razorpay.Client(
+            auth=("rzp_live_4jh8hMv539N0rm", "RtVNts9SZLXkCW4yeJ8e2Yro"))
+
+    payment = client.order.create({'amount': amount, 'currency': 'INR',
+                                       'payment_capture': '1'})
     return render(request, 'index.html')
 
 @csrf_exempt
