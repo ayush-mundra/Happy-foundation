1a05d1e7de74f0a6961f881505ce9b9eafe1d404
JAgrit20 <jagritacharya2020@gmail.com>
2021-01-19 11:39:56 +0530

    Ngo home page created and route added

--- ngo/views.py
+++ ngo/views.py
@@ -1,3 +1,13 @@
+
+from django.contrib import  admin
+from django.urls import path, include
+from donator import views
+from django.conf import settings
+from django.conf.urls.static import static
+from donator.models import Product
 from django.shortcuts import render
 
-# Create your views here.
+
+def homes(request):
+    products = Product.objects
+    return render(request,'ngopages/home.html',{'products':products})
