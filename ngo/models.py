a97baf34514655f34c98d2257d9780210b84ff20
JAgrit20 <jagritacharya2020@gmail.com>
2021-01-20 09:48:39 +0530

    Ngo Model created

--- ngo/models.py
+++ ngo/models.py
@@ -7,8 +7,8 @@ class Ngo(models.Model):
     Name = models.CharField(max_length=200,default='SOME STRING')
     Phone = models.CharField(max_length=20,default='SOME STRING')
     body = models.TextField()
+    Address = models.TextField()
     Weburl = models.TextField()
-
-    itemImage = models.ImageField(upload_to='images/')
-    Id = models.ImageField(upload_to='images/',default='SOME STRING')
+    insideImage = models.ImageField(upload_to='Ngoimages/')
+    Id = models.ImageField(upload_to='Id/',default='SOME STRING')
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
--- ngo/views.py
+++ ngo/views.py
@@ -1,13 +1,29 @@
 
 from django.contrib import  admin
 from django.urls import path, include
-from donator import views
+from ngo import views
 from django.conf import settings
 from django.conf.urls.static import static
 from donator.models import Product
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 
 
 def homes(request):
     products = Product.objects
     return render(request,'ngopages/home.html',{'products':products})
+
+def create(request):
+    if request.method == 'POST':
+        if request.POST['title'] and request.POST['name'] and request.POST['phone'] and request.POST['address'] and request.FILES['itemimage'] and request.FILES['id']:
+            product = Product()
+            product.title = request.POST['title']
+            product.Name = request.POST['name']
+            product.body = request.POST['body']
+            product.Phone = request.POST['phone']
+            product.address = request.POST['address']
+            product.Id = request.FILES['id']
+            product.itemImage = request.FILES['itemimage']
+            product.pub_date = timezone.datetime.now()
+            product.owner = request.user
+            product.save()
+            return redirect('/donator/'+ str(product.id))
