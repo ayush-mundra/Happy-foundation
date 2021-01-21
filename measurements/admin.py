c084380af67c9a61f46c7517c09ae5bd7d40f0ac
JAgrit20 <jagritacharya2020@gmail.com>
2021-01-21 19:26:53 +0530

    distance cal in process

--- measurements/admin.py
+++ measurements/admin.py
@@ -3,4 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from .models import Measurement
 
+
+
 admin.site.register(Measurement)
+
--- measurements/apps.py
+++ measurements/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class MeasurementsConfig(AppConfig):
     name = 'measurements'
+    verbose_name ='distance between the 2 locations '
