ed2b81efa713d665864261fc08b344ba1dd04e46
JAgrit20 <jagritacharya2020@gmail.com>
2021-01-22 18:43:52 +0530

    model updated

--- measurements/models.py
+++ measurements/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Measurement(models.Model):
     location  = models.CharField(max_length=200)
+    UserName  = models.CharField(max_length=200)
     destination = models.CharField(max_length=200)
     distance = models.DecimalField( max_digits=10, decimal_places=2)
     created = models.DateField( auto_now_add=True)
