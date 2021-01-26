b7f906ed0b89651d748a6ee29a34e2893d7ff6fc
JAgrit20 <jagritacharya2020@gmail.com>
2021-01-26 18:36:42 +0530

    countries added

--- Userprofile/models.py
+++ Userprofile/models.py
@@ -3,11 +3,16 @@
 # name = request.POST.get('Fname'),userimage = request.FILES.get('userimg')
 from django.db import models
 from django.contrib.auth.models import User
+# from django_countries.fields import CountryField
+
+
+
 
 # Create your models here.
-class Product(models.Model):
+class Profile(models.Model):
    Name = models.CharField(max_length=200,default='SOME STRING')
    Phone = models.CharField(max_length=20,default='SOME STRING')
    address = models.CharField(max_length=200,default='SOME STRING')
    Userimg = models.ImageField(upload_to='User/')
+   # country = models.CountryField()
 
