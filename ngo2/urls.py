81d23f1cf8680197b9300a928fcdfbea5756e199
JAgrit20 <jagritacharya2020@gmail.com>
2021-05-06 07:33:49 +0530

    dp changes

--- ngo2/urls.py
+++ ngo2/urls.py
@@ -4,7 +4,7 @@ from django.urls import path,include
 from donator import views
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import url
+from django.conf.urls import url 
 
 # urlpatterns = [
 #     path('admin/', admin.site.urls),
